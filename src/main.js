0a4a501c6a7a6fe79f3f6e316d1942093a3eee3d
Paul Sheehan <p.sheehan94@gmail.com>
2021-02-17 01:18:25 +0000

    Added sitemap etc, and setup ganalytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Clipboard from "v-clipboard";
+import VueAnalytics from "vue-ua";
 
 Vue.config.productionTip = false;
-Vue.use(Clipboard);
+Vue.use(Clipboard, VueAnalytics, {
+  appName: "link-shortener-c40d2",
+  appVersion: "1",
+  trackingId: process.env.API_KEY,
+});
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
